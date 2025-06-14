396a49b5e1599f1144c4f503db267d6c8e9e1d70
Giuseppe Buzzanca <giuseppe.buzzanca@accenture.com>
2025-06-14 10:07:38 +0200

    Fixed ScrollView dimension

--- app/picture/PictureScreen.tsx
+++ app/picture/PictureScreen.tsx
@@ -7,9 +7,9 @@ type PictureNativeStackScreenProps = NativeStackScreenProps<PicturesStackParamLi
 
 export default function PictureScreen({ route }: PictureNativeStackScreenProps) {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Image style={{ aspectRatio: '16/9' }} source={{ uri: route.params?.apod?.hdurl }} loadingIndicatorSource={require('../assets/loading.png')}></Image>
-      <ScrollView style={{ padding: 16 }}>
+      <ScrollView style={{ paddingStart: 16, paddingEnd: 16, flex: 1 }}>
         <Text style={{ color: "white", fontWeight: "bold", fontSize: 22, marginTop: 16 }}>{route.params?.apod?.title}</Text>
         <Text style={{ color: "#8F99CC", fontSize: 14, marginTop: 16 }}>{route.params?.apod?.date}</Text>
         <Text style={{ color: "white", fontSize: 16, marginTop: 16, marginBottom: 16 }}>{route.params?.apod?.explanation}</Text>
